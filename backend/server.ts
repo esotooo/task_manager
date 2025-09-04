c4e893fc5de5e94bbf1eb63af3b7f0c181d2b89e
esotooo <edwinsotos2003@gmail.com>
2025-09-03 22:35:14 -0600

    chore: agregando cambios restantes, incluyendo interface LoginType en backend/types

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import registerAPI from './API/users/register';
+import loginAPI from './API/users/login';
 
 const PORT = 4000;
-const app = express()
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', registerAPI);
+app.use('/api/users', loginAPI);
+
 
 app.listen(PORT, () => {
-    console.log(`Aplicación corriendo en puerto http://localhost:${PORT}`)
+    console.log(`Aplicación corriendo en puerto http://localhost:${PORT}`);
 })
