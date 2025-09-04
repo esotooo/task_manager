71046b43c43ed5e86f1137e9a947b6a832bb729b
esotooo <edwinsotos2003@gmail.com>
2025-09-03 22:26:08 -0600

    feat: agregar funciones hashPassword y responseHandler en utils

---
+++ backend/utils/responseHandler.ts
@@ -0,0 +1,28 @@
+import { Response } from "express";
+
+type apiResponse = {
+    success: boolean,
+    data?: any, 
+    message: string, 
+    error?: any,
+    token?: any | null
+}
+
+export function sendSucess(res: Response, statusCode: number, data: any, message = "Sucess", token?: any){
+    const response : apiResponse = {
+        success: true,
+        data,
+        message,
+        token
+    }
+    return res.status(statusCode).json(response);
+}
+
+export function sendError(res: Response, statusCode: number, message: string, error: any = null){    
+    const response : apiResponse = {
+        success: false, 
+        message, 
+        error
+    }
+    return res.status(statusCode).json(response)
+}
