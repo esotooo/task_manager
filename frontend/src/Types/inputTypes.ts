d6c86ee70fbb4975a03656d1ba72f4ff6d7d3358
esotooo <edwinsotos2003@gmail.com>
2025-09-09 17:36:32 -0600

    feat: crear Props para tipado de input personalizados creados en ReusableInput.tsx

---
+++ frontend/src/Types/inputTypes.ts
@@ -0,0 +1,20 @@
+export type FormInputProps = {
+    type?: 'text' | 'email' | 'password' | 'number' | 'tel'
+    placeholder: string
+    name: string
+    value: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    error?: string | null
+    className?: string
+}
+
+export type PasswordInputProps = {
+    placeholder: string
+    name?: string
+    value: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    error?: string | null
+    visible: boolean
+    onToggleVisibility: (e: React.MouseEvent<HTMLDivElement>) => void
+    className?: string
+}
