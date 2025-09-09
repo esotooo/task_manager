78a499c7419f1d90f86484fec10d05a193afc2f5
esotooo <edwinsotos2003@gmail.com>
2025-09-09 14:13:57 -0600

    style: agregar estilos para una mejor visualización en dispositivos moviles

--- frontend/src/Pages/Auth/registerUser.tsx
+++ frontend/src/Pages/Auth/registerUser.tsx
@@ -4,10 +4,10 @@ import { SiTask } from "react-icons/si";
 export default function registerUser() {
 
     return (
-        <div className="w-screen h-screen flex flex-col items-center py-20">
+        <div className="w-screen h-screen flex flex-col items-start sm:py-20 sm:items-center py-10 px-5">
             <div className="flex flex-row items-center">
-                <SiTask className="text-7xl"/>
-                <h1 className="text-6xl whitespace-nowrap">
+                <SiTask className="sm:text-7xl text-5xl"/>
+                <h1 className="sm:text-6xl text-5xl">
                 Task <span className="font-extrabold -ml-2">Flow</span></h1>  
             </div>
             <RegisterUserForm />
