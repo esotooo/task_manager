00b9cacd0f1ee0c3c8e8b85519e5c47d957b6e3f
esotooo <edwinsotos2003@gmail.com>
2025-09-10 11:11:35 -0600

    feat: agregar variables onBlur y onFocus a PasswordInputProps

--- frontend/src/Types/inputTypes.ts
+++ frontend/src/Types/inputTypes.ts
@@ -12,9 +12,11 @@ export type PasswordInputProps = {
     placeholder: string
     name?: string
     value: string
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
     error?: string | null
     visible: boolean
     onToggleVisibility: (e: React.MouseEvent<HTMLDivElement>) => void
     className?: string
+    onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void
+    onFocus?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
