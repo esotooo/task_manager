6eeb925ffb22699965b65b4c78e23bb32e99fa8d
esotooo <edwinsotos2003@gmail.com>
2025-09-16 17:59:41 -0600

    chore: eliminar estados y funciones que no son necesarios en el estado global

--- frontend/src/Context/AuthContext.tsx
+++ frontend/src/Context/AuthContext.tsx
@@ -1,140 +1,56 @@
-import {createContext, useReducer, useState, type ReactNode} from 'react'
-import {AuthReducer, type AuthActions, initialAuthState, type AuthState}from '../Reducers/auth-reducer.ts'
-import { api } from '../Utils/axiosInstance.ts'
-import {useNavigate } from "react-router-dom"
-import type {LoginType, RegisterType } from '../Types/authTypes.ts'
-
-
+import { createContext, useState, type ReactNode } from 'react';
+import { api } from '../Utils/axiosInstance.ts';
+import type { InformationReceivedType, LoginType } from '../Types/authTypes.ts';
 
 type AuthContextProps = {
-    state: AuthState,
-    dispatch: React.Dispatch<AuthActions>,
-    login: (body: LoginType) => Promise<void>,
-    token: string,
-    setToken: (token: string) => void,
-    registerNewUser: (body: RegisterType) => Promise<boolean>,
-    navigateTo: (path: string) => (e: React.MouseEvent) => void,
-    fieldMessage: string,
-    showError: boolean,
-    showConfirm: boolean,
-    searchExistingUsernames: (username: string) => Promise<void>,
-    suggestions: string[],
-    setSuggestions: (suggestions: string[]) => void
+  user: InformationReceivedType | null
+  login: (body: LoginType) => Promise<loginResult>
+  logout: () => void
+};
+
+
+type loginResult = {
+    success: boolean;
+    fields?: Record<string, string> 
+    message?: string 
 }
 
 export const AuthContext = createContext<AuthContextProps | undefined>(undefined)
 
-export const AuthProvider = ({children}: {children: ReactNode}) => {
-    const [state, dispatch] = useReducer(AuthReducer, initialAuthState);
-    const [token, setToken] = useState('')
-    const [fieldMessage, setFieldMessage] = useState('')
-    const [showConfirm, setShowConfirm] = useState(false)
-    const [showError, setShowError] = useState(false)
-    const [suggestions, setSuggestions] = useState<string[]>([])
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<InformationReceivedType | null>(null)
 
-    const navigate = useNavigate();
-
-    const handleError = (error:any) => {
-        dispatch({type: 'show-message', payload: {message: error.response.data.message || 'Error en el servidor.'}});
-        setTimeout(() => {
-            dispatch({type: 'close-message', payload: {message: ''}})
-        }, 3000);
-    }
-
-    const registerErrors = (msg : string) => {
-        dispatch({type: 'show-message', payload: {message: msg}})
-        setShowError(true)
-        setTimeout(() => {
-            dispatch({ type: 'close-message', payload: { message: '' } })
-            setShowError(false)
-        }, 5000);
-    }
-
-    const login = async (user: LoginType) => {
-        try{
-            const res = await api.post('/api/users/login', user)
-            if(res.status === 200){
-                dispatch({type: 'login', payload: {login: res.data.data}});
-                navigate('/main', {replace: true, state: {login: res.data.data}}); 
-                setToken(res.data.token)           
-            }
-        }
-        catch(error){
-            handleError(error)
-        }
-    }
-
-    const searchExistingUsernames = async(username : string) => {
+    const login = async (credentials: LoginType): Promise<loginResult> => {
         try {
-            const res = await api.get(`/api/users/search-username?username=${username}`)
+            const res = await api.post('/api/users/login', credentials)
             if (res.status === 200) {
-                setSuggestions([])
+                setUser(res.data)
+                return {success: true}
             }
-            return true
-        } catch (error: any) {
-            if (error.response) {
-                if (error.response.status === 409) {
-                    setSuggestions(error.response.data.data.suggestions)
-                    return false
+            return {success: false}
+        } catch(error : any) {
+            if(error.response){
+                if(error.response.data.fields){
+                    return {success: false, fields: error.response.data.fields}
+                }if(error.response.data.message){
+                    return {success: false, message: error.response.data.message}
                 }
-            } 
-            setSuggestions([])
-            return false
+            }
+            return {success: false, message: 'Error en la conexión con el servidor.'}
         }
     }
-    
 
-    const registerNewUser = async (newUser: RegisterType) => {
-        try {
-            const res = await api.post('/api/users/register', newUser);
-        
-            if (res.status === 201) {
-                setShowConfirm(true)
-                dispatch({ type: 'show-message', payload: { message: 'Usuario registrado exitosamente.' } })
-        
-                setTimeout(() => {
-                    navigate('/login', { replace: true })
-                    dispatch({ type: 'close-message', payload: { message: '' } })
-                    setShowConfirm(false)
-                    setFieldMessage('')
-                }, 5000)
-
-                return true
-            }
-        
-            return false
-
-        } catch (error: any) {
-            if(error.response){
-                if(error.response?.data?.fields){
-                    setFieldMessage(error.response.data.fields)
-                } else if(error.response?.data?.message){
-                    registerErrors(error.response.data.message)
-                }
-            }else{
-                const msg = 'No hay respuesta del servidor.'
-                registerErrors(msg)
-            }
-
-            return false
-
-            }
+    const logout = () => {
+        setUser(null)
     }
 
-
-      
-    const navigateTo = (path: string) => (e: React.MouseEvent) => {
-        e.preventDefault()
-        navigate(path)
-    }
-
-    return(
-        <AuthContext.Provider 
-            value={{state, dispatch, login, setToken, token, registerNewUser, navigateTo, fieldMessage, showConfirm, showError, searchExistingUsernames, suggestions, setSuggestions}}    
-        >
+    return (
+        <AuthContext.Provider value={{
+            user, 
+            login, 
+            logout 
+        }}>
             {children}
         </AuthContext.Provider>
     )
 }
-
-
