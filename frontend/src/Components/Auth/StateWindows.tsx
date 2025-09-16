d0d6f8f49daa55b77bb16f75945729a2c7813a71
esotooo <edwinsotos2003@gmail.com>
2025-09-16 17:53:05 -0600

    feat: agregar lógica de animación en el componente y restructurando código

--- frontend/src/Components/Auth/StateWindows.tsx
+++ frontend/src/Components/Auth/StateWindows.tsx
@@ -1,29 +1,39 @@
-import { useRegisterForm } from "../../Hooks/useRegisterForm"
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
-export default function StateWindows() {
+type StateWindowsProps = {
+  showConfirm: boolean;
+  showError: boolean;
+  message: string | null;
+  container: HTMLDivElement | null;
+}
 
-    const {showConfirm, showError, container, state} = useRegisterForm()
+export default function StateWindows({ showConfirm, showError, message }: StateWindowsProps) {
+    const container = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if ((showConfirm || showError) && container.current) {
+        const animInstance = lottie.loadAnimation({
+            container: container.current,
+            renderer: "svg",
+            loop: false,
+            autoplay: true,
+            path: showConfirm ? "/gif/Success.json" : "/gif/error.json",
+        });
+
+        return () => animInstance.destroy();
+        }
+    }, [showConfirm, showError]);
+    
+
+    if (!showConfirm && !showError) return null;
 
     return (
-        <>
-            {showConfirm && (
-                <div className="absolute inset-0 bg-white flex flex-col rounded-lg z-10">
-                    <div className="flex items-center p-4">
-                        <div className="w-15 h-15" ref={container}></div>
-                        <p className="font-bold">{state.message}</p>
-                    </div>
-                </div>
-            )}
-
-            {showError &&(
-                <div className="absolute inset-0 bg-white flex flex-col rounded-lg z-10">
-                    <div className="flex items-center p-4">
-                        <div className="w-15 h-15" ref={container}></div>
-                        <p className="font-bold">{state.message}</p>
-                    </div>
-                </div>
-            )}
-
-    </>
+        <div className="absolute inset-0 bg-white flex flex-col rounded-lg z-10">
+            <div className="flex items-center p-4">
+                <div className="w-15 h-15" ref={container}></div>
+                <p className="font-bold">{message}</p>
+            </div>
+        </div>
     )
 }
