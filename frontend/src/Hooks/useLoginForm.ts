3613175636a4fce62d47631c6935b5a12824fc88
esotooo <edwinsotos2003@gmail.com>
2025-09-16 18:06:32 -0600

    feat: agregar funciones para el formulario de login

--- frontend/src/Hooks/useLoginForm.ts
+++ frontend/src/Hooks/useLoginForm.ts
@@ -1,15 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import {useAuth} from './useAuth'
 import { useState } from 'react'
-import type { LoginType } from "../Types/authTypes";
 
+type FieldsErrors = { [key: string]: string };
 
 export const useLoginForm = () => {
     const {login} = useAuth()
     const [form, setForm] = useState({
         loginInput: '',
-        password: ''
+        user_password: ''
     })
     const [passwordVisible, setPasswordVisible] = useState(false)
+    const [fieldMessage, setFieldMessage] = useState<{ [key: string]: string }>({});
+    const [showError, setShowError] = useState(false);
+    const [message, setMessage] = useState<string | null>(null)
+
+    const navigate = useNavigate()
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target
@@ -21,27 +27,44 @@ export const useLoginForm = () => {
 
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const {loginInput, password} = form
+        const result = await login(form)
 
-        const body:LoginType = {user_password: password}
-        if(form.loginInput.includes('@')){
-            body.email = loginInput
+        if(result.success){
+            navigate('/main', {replace: true})
+            setFieldMessage({})
+            setMessage(null)
+            setShowError(false)
         }else{
-            body.username = loginInput
+            setFieldMessage(result.fields || {})
+            setMessage(result.message || null)
+            setShowError(true)
         }
-        await login(body)
     }
 
     const togglePasswordVisibility = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.preventDefault()
         setPasswordVisible(prev => !prev)
     }
-    
+
+    const getFieldsError = (fieldname: string) => {
+        if (!fieldMessage) return null
+        const fields = fieldMessage as unknown as FieldsErrors
+        return fields[fieldname] || null
+    }
+
+    const navigateTo = (page: string) => {
+        navigate(page)
+    }
+
     return{
         form,
         passwordVisible,
+        message,
+        showError,
         handleChange,
         handleLogin,
-        togglePasswordVisibility
+        togglePasswordVisibility,
+        getFieldsError,
+        navigateTo
     }
 }
