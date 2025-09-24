9ca9cdf5ee62365064e89bb29de85d7c69498f32
esotooo <edwinsotos2003@gmail.com>
2025-09-23 19:12:33 -0600

    refactor: reorganizar customs hooks a carpetas segun su clasificacion

---
+++ frontend/src/Hooks/Auth/useLogin.ts
@@ -0,0 +1,66 @@
+import { useNavigate } from 'react-router-dom';
+import {useAuth} from './useAuth'
+import { useState } from 'react'
+import { useInputError } from '../Layout/useInputError';
+
+
+export const useLoginForm = () => {
+    const {login}  = useAuth()
+    const [form, setForm] = useState({
+        loginInput: '',
+        user_password: ''
+    })
+    const [passwordVisible, setPasswordVisible] = useState(false)
+    const [showError, setShowError] = useState(false);
+    const [message, setMessage] = useState<string | null>(null)
+    const {setFieldMessage, clearFieldsError, getFieldsError} = useInputError()
+
+    const navigate = useNavigate()
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setForm(prev => ({
+            ...prev, 
+            [name]: value
+        }))
+    }
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const result = await login(form)
+
+        if(result.success){
+            navigate('/main', {replace: true})
+            setFieldMessage({})
+            setMessage(null)
+            setShowError(false)
+        }else{
+            setFieldMessage(result.fields || {})
+            setMessage(result.message || null)
+            setShowError(true)
+        }
+    }
+
+    const togglePasswordVisibility = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.preventDefault()
+        setPasswordVisible(prev => !prev)
+    }
+
+
+    const navigateTo = (page: string) => {
+        navigate(page)
+    }
+
+    return{
+        form,
+        passwordVisible,
+        message,
+        showError,
+        handleChange,
+        handleLogin,
+        togglePasswordVisibility,
+        getFieldsError,
+        clearFieldsError,
+        navigateTo
+    }
+}
