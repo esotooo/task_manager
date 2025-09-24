41ff488a54e25d874ee2b2f4804044f257e69fe6
esotooo <edwinsotos2003@gmail.com>
2025-09-23 19:07:50 -0600

    refactor: actualizar directorio de custom hook useAuth

--- frontend/src/Components/Private/ProtectedMain.tsx
+++ frontend/src/Components/Private/ProtectedMain.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../../Hooks/useAuth";
+import { useAuth } from "../../Hooks/Auth/useAuth";
 
 type ProtectedMainProps = {
     children: React.JSX.Element
