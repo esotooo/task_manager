19b8c66bc86879bb8d28fe10507f17b63425f615
esotooo <edwinsotos2003@gmail.com>
2025-09-23 19:11:45 -0600

    feat: crear componente para validar el flujo de los componentes para cambiar contraseña

---
+++ frontend/src/features/ChangePasswordFlow.tsx
@@ -0,0 +1,13 @@
+import EmailForm from "../Components/Auth/EmailForm";
+import VerifyTokenForm from "../Components/Auth/VerifyTokenForm";
+import { useChangePassword } from "../Hooks/Auth/useChangePassword";
+
+export default function ChangePasswordFlow() {
+    const {step} = useChangePassword()
+
+    if (step === 'email') return <EmailForm />
+    if (step === 'otp') return <VerifyTokenForm />
+    if (step === 'change') return <ChangePasswordFlow />
+
+    return null
+}
