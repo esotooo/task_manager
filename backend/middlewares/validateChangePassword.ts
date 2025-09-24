1c7437da82f715938863435ac7eb545ccc2e1d98
esotooo <edwinsotos2003@gmail.com>
2025-09-23 18:58:38 -0600

    feat: agregar variable para validación por campos para API 'verify-otp'

--- backend/middlewares/validateChangePassword.ts
+++ backend/middlewares/validateChangePassword.ts
@@ -5,12 +5,25 @@ export const validateEmail = [
     body('email')
         .trim()
         .notEmpty()
-        .withMessage('El correo electrónico es obligatorio.')
+        .withMessage('Por favor, ingrese su correo electrónico.')
         .bail()
         .isEmail()
-        .withMessage('El correo electrónico ingresado no tiene un formato válido.')
+        .withMessage('Ingrese un correo electrónico válido.')
         .bail(),
 ]
+
+export const validateOTP = [
+    body('otp')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese el código de verificación enviado a su correo.')
+        .bail()
+        .isLength({min: 6, max: 6})
+        .withMessage('El código debe tener exactamente 6 dígitos.')
+        .bail()
+        .matches(/^[0-9]+$/)
+        .withMessage('El código solo puede contener números.')
+    ]
 
 export const handleValidationErrorsByField = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
