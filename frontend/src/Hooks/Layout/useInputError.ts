a372f13105e8d6aedc99adb6103bcf04ec4ae58e
esotooo <edwinsotos2003@gmail.com>
2025-09-23 19:13:08 -0600

    feat: crear custom Hook para manejo de errores por cada input

---
+++ frontend/src/Hooks/Layout/useInputError.ts
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+type FieldsErrors = {
+  [key: string]: string
+}
+
+export const useInputError = (initialErrors: FieldsErrors = {}) => {
+  const [fieldMessage, setFieldMessage] = useState<FieldsErrors>(initialErrors)
+
+  const getFieldsError = (fieldname: string) => {
+    if (!fieldMessage) return null
+    const fields = fieldMessage as unknown as FieldsErrors
+    return fields[fieldname] || null
+  }
+
+  const clearFieldsError = (fieldName: string, msg: string) => {
+    setFieldMessage(prev => ({ ...prev, [fieldName]: msg }))
+  }
+
+
+  return { fieldMessage, getFieldsError, clearFieldsError, setFieldMessage };
+};
