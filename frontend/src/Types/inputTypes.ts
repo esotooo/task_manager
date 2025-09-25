537474a486b073a0282309224e27f2595c1e4c43
esotooo <edwinsotos2003@gmail.com>
2025-09-25 11:39:43 -0600

    fix: eliminar tipado para visibilidad de contraseña

--- frontend/src/Types/inputTypes.ts
+++ frontend/src/Types/inputTypes.ts
@@ -23,11 +23,9 @@ export type PasswordInputProps = {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
     setError?: (msg: string) => void
     error?: string | null
-    visible: boolean
-    onToggleVisibility: (e: React.MouseEvent<HTMLDivElement>) => void
     className?: string
     onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void
     onFocus?: (e: React.ChangeEvent<HTMLInputElement>) => void
     requirements?: PasswordRequirement[]
-    showPasswordRequirements?: boolean
+    showPasswordRequirements?: boolean,
 }
