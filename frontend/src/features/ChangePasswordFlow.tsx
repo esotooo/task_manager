170a2c23a80f8923cee391abe6b1b0c5616e50f6
esotooo <edwinsotos2003@gmail.com>
2025-09-24 19:48:38 -0600

    fix: corrigir componente ChangePasswordFlow a ChangePasswordForm

--- frontend/src/features/ChangePasswordFlow.tsx
+++ frontend/src/features/ChangePasswordFlow.tsx
@@ -1,3 +1,4 @@
+import ChangePasswordForm from "../Components/Auth/ChangePasswordForm";
 import EmailForm from "../Components/Auth/EmailForm";
 import VerifyTokenForm from "../Components/Auth/VerifyTokenForm";
 import { useChangePassword } from "../Hooks/Auth/useChangePassword";
@@ -7,7 +8,7 @@ export default function ChangePasswordFlow() {
 
     if (step === 'email') return <EmailForm />
     if (step === 'otp') return <VerifyTokenForm />
-    if (step === 'change') return <ChangePasswordFlow />
+    if (step === 'change') return <ChangePasswordForm />
 
     return null
 }
