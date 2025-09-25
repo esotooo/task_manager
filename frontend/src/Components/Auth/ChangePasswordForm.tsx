70655c1d44c56c641ee0a2e52de2efeaf3e104bc
esotooo <edwinsotos2003@gmail.com>
2025-09-24 18:44:41 -0600

    feat: crear componente para cambiar la contraseña del correo electronico ingresado

---
+++ frontend/src/Components/Auth/ChangePasswordForm.tsx
@@ -0,0 +1,39 @@
+import { useChangePassword } from "../../Hooks/Auth/useChangePassword";
+import { PasswordInput } from "../Layout/ReusableInput";
+
+export default function ChangePasswordForm() {
+
+    const {handleChangePassword, form, handleChange,
+        togglePasswordVisibility, passwordVisible, getFieldsError, clearFieldsError
+    } = useChangePassword()
+        
+    return (
+        <div>
+            <p className="text-xs text-gray-500 mb-8">
+                Ingresa tu nueva contraseña para completar el proceso de restablecimiento de tu perfil. 
+                Asegúrate de elegir una contraseña segura que solo tú conozcas.
+            </p>
+            <form onSubmit={handleChangePassword}>
+                    <PasswordInput 
+                        placeholder="Contraseña nueva"
+                        name="user_password"
+                        value={form.user_password}
+                        onChange={handleChange}
+                        onToggleVisibility={togglePasswordVisibility}
+                        visible={passwordVisible}
+                        error={getFieldsError('user_password')}
+                        setError={() => clearFieldsError('user_password', '')}
+                        showPasswordRequirements
+                    />
+
+                <button 
+                    className="cursor-pointer bg-black text-white mt-8 font-bold
+                    w-full py-3 text-sm rounded-md hover:bg-black/80" 
+                    type="submit"
+                >
+                    Cambiar contraseña
+                </button>
+            </form>
+        </div>
+    )
+}
