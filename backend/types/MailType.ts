6387b732a52d6bbb9d37eecce63b10dfb6cec9d8
esotooo <edwinsotos2003@gmail.com>
2025-09-29 16:26:34 -0600

    feat: agregar tipado para token enviado por medio de correo electronico para validar el usuario

--- backend/types/MailType.ts
+++ backend/types/MailType.ts
@@ -2,14 +2,20 @@ import { RowDataPacket } from "mysql2";
 
 export type MailType = {
     email: string,
-    firstname: string,
-    lastname: string,
-    otp: string,
+    firstname?: string,
+    lastname?: string,
+    otp?: string,
+    verificationUrl?: string
 };
 
 export type OTPType = {
     [email: string]: { code: string; expiresAt: number };
 };
+
+export type TokenType = {
+    [email: string]: {token: string, expiresAt: number};
+};
+
 
 export interface GetEmailType extends RowDataPacket{
     email: string,
