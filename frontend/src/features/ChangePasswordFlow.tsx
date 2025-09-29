82946ed96cdedfec27ad8142f887b5dd58ae394d
esotooo <edwinsotos2003@gmail.com>
2025-09-29 12:29:39 -0600

    fix: corregir variable de form a state debido al use-reducer implementado

--- frontend/src/features/ChangePasswordFlow.tsx
+++ frontend/src/features/ChangePasswordFlow.tsx
@@ -4,11 +4,11 @@ import VerifyTokenForm from "../Components/Auth/VerifyTokenForm";
 import { useChangePassword } from "../Hooks/Auth/useChangePassword";
 
 export default function ChangePasswordFlow() {
-    const {step} = useChangePassword()
+    const {state} = useChangePassword()
 
-    if (step === 'email') return <EmailForm />
-    if (step === 'otp') return <VerifyTokenForm />
-    if (step === 'change') return <ChangePasswordForm />
+    if (state.step === 'email') return <EmailForm />
+    if (state.step === 'otp') return <VerifyTokenForm />
+    if (state.step === 'change') return <ChangePasswordForm />
 
     return null
 }
