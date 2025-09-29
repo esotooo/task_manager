1ba50e54badb4c83d19b809ac1837c4b91c96e8d
esotooo <edwinsotos2003@gmail.com>
2025-09-29 16:11:27 -0600

    fix: actualizar nombre de variables y modificar lógica para la creación de correos

--- backend/utils/emailGenerator.ts
+++ backend/utils/emailGenerator.ts
@@ -4,19 +4,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export async function sendEmail({ email, firstname, lastname, otp }: MailType) {
+export const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_MAIL,
+        pass: process.env.GMAIL_PASS
+    },
+});
+
+export async function sendChangePasswordEmail({ email, firstname, lastname, otp }: MailType) {
     try {
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.GMAIL_MAIL,
-                pass: process.env.GMAIL_PASS
-            },
-        });
+
+        const emailNormalized = email.trim().toLowerCase()
 
         const mailConfigs = {
             from: `"Task Flow" <${process.env.GMAIL_MAIL}>`,
-            to: email.trim(),
+            to: emailNormalized,
             subject: `Código de verificación.`,
             text: `¡Hola ${firstname} ${lastname}! Tu código de verificación es: ${otp}. Es válido por 5 minutos.`
         };
@@ -24,6 +27,25 @@ export async function sendEmail({ email, firstname, lastname, otp }: MailType) {
         const info = await transporter.sendMail(mailConfigs);
         return info; 
     } catch (error) {
+        throw error;
+    }
+}
+
+export async function sendVerificationEmail({email, verificationUrl} : MailType){
+    try{
+
+        const emailNormalized = email.trim().toLowerCase()
+
+        const mailConfigs = {
+            from: `"Task Flow" <${process.env.GMAIL_MAIL}>`,
+            to: emailNormalized,
+            subject: `Verificación de correo electrónico.`,
+            html: `<a href="${verificationUrl}">Verificar correo</a>`
+        };
+
+        const info = await transporter.sendMail(mailConfigs);
+        return info;
+    }catch (error){
         throw error;
     }
 }
