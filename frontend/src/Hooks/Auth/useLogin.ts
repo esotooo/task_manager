1586f035ab2d7a1a73caea048d9f1359784b59cc
esotooo <edwinsotos2003@gmail.com>
2025-09-29 12:27:04 -0600

    refactor: eliminar multiples estados para agruparlos en un use-reducer

--- frontend/src/Hooks/Auth/useLogin.ts
+++ frontend/src/Hooks/Auth/useLogin.ts
@@ -1,43 +1,43 @@
 import { useNavigate } from 'react-router-dom';
 import {useAuth} from './useAuth'
-import { useState } from 'react'
+import { useReducer } from 'react'
 import { useInputError } from '../Layout/useInputError';
+import { LoginReducer, initialState} from '../../Reducers/login-reducer';
 
 
 export const useLoginForm = () => {
-    const {login}  = useAuth()
-    const [form, setForm] = useState({
-        loginInput: '',
-        user_password: ''
-    })
-    const [showError, setShowError] = useState(false);
-    const [message, setMessage] = useState<string | null>(null)
 
+    // Variable para guardar token y datos en un estado global
+    const { login }  = useAuth() 
+
+    // useReducer para manejo de acciones dentro del formulario de login
+    const [ state, dispatch ] = useReducer(LoginReducer, initialState)
+
+    // custom hook para manejo de errores por campo
     const {setFieldMessage, clearFieldsError, getFieldsError} = useInputError()
 
     const navigate = useNavigate()
 
+    //--- FUNCIONES: FORMULARIO ----
+    // Funcion para detectar cambios en input
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target
-        setForm(prev => ({
-            ...prev, 
-            [name]: value
-        }))
+        dispatch({type: 'SET_FIELD', field: e.target.name, value: e.target.value})
     }
 
+    // Funcion para iniciar sesión por medio de un formulario
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const result = await login(form)
+        const result = await login(state.form)
 
         if(result.success){
             navigate('/main', {replace: true})
             setFieldMessage({})
-            setMessage(null)
-            setShowError(false)
+            dispatch({type: 'CLEAR_ERROR'})
+            dispatch({type: 'CLEAR_FIELDS'})
+
         }else{
             setFieldMessage(result.fields || {})
-            setMessage(result.message || null)
-            setShowError(true)
+            dispatch({type: 'SET_ERROR', message: result.message || null})
         }
     }
 
@@ -46,13 +46,11 @@ export const useLoginForm = () => {
     }
 
     return{
-        form,
-        message,
-        showError,
+        state,
         handleChange,
         handleLogin,
         getFieldsError,
         clearFieldsError,
-        navigateTo
+        navigateTo,
     }
 }
