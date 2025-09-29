08f07c7f4da5e6541ac3e3083d8a895f7b16d5d3
esotooo <edwinsotos2003@gmail.com>
2025-09-29 12:18:18 -0600

    feat: agregar variables extra al tipado

--- backend/types/usersTypes.ts
+++ backend/types/usersTypes.ts
@@ -4,6 +4,8 @@ export interface LoginType extends RowDataPacket{
     id_user: number, 
     firstname: string, 
     lastname: string,
+    username: string,
+    email: string
 }
 
 export interface GetUsernameType extends RowDataPacket{
