b006a07867a6440ec497fc4162e63ead9107ebb6
esotooo <edwinsotos2003@gmail.com>
2025-09-29 12:16:23 -0600

    feat: agregar mas campos en la consulta SQL

--- backend/Queries/Auth/usersQueries.ts
+++ backend/Queries/Auth/usersQueries.ts
@@ -1,6 +1,6 @@
-const loginByEmailQuery = `SELECT id_user, firstname, lastname, user_password FROM users WHERE email = ?`;
+const loginByEmailQuery = `SELECT id_user, firstname, lastname, user_password, username, email FROM users WHERE email = ?`;
 
-const loginByUsernameQuery = `SELECT id_user, firstname, lastname, user_password FROM users WHERE username = ?`;
+const loginByUsernameQuery = `SELECT id_user, firstname, lastname, user_password, username, email FROM users WHERE username = ?`;
 
 export const usersQueries = {
     loginByEmailQuery: loginByEmailQuery,
