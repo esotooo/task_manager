83d3f089cd4ab5051a6d17add75cb0d8b3a18a74
esotooo <edwinsotos2003@gmail.com>
2025-10-02 15:50:51 -0600

    feat: agregar ventana para mostrar el estado de la acción al cambiar la contraseña

--- frontend/src/Components/Auth/ChangePasswordForm.tsx
+++ frontend/src/Components/Auth/ChangePasswordForm.tsx
@@ -1,39 +1,54 @@
 import { useChangePassword } from "../../Hooks/Auth/useChangePassword";
 import { PasswordInput } from "../Layout/ReusableInput";
+import StateWindows from "./StateWindows";
 
 export default function ChangePasswordForm() {
 
-    const {handleChangePassword, state, handleChange,
-        getFieldsError, clearFieldsError
+    const {
+        handleChangePassword, 
+        state, 
+        container,
+        handleChange,
+        getFieldsError, 
+        clearFieldsError,
     } = useChangePassword()
         
     return (
         <div>
+            <div className="relative">
+
             <p className="text-xs text-gray-500 mb-8">
                 Ingresa tu nueva contraseña para completar el proceso de restablecimiento de tu perfil. 
                 Asegúrate de elegir una contraseña segura que solo tú conozcas.
             </p>
-            <form onSubmit={handleChangePassword}>
-                <PasswordInput 
-                    placeholder="Contraseña nueva"
-                    name="user_password"
-                    value={state.form.user_password}
-                    onChange={handleChange}
-                    error={getFieldsError('user_password')}
-                    setError={() => clearFieldsError('user_password', '')}
-                    showPasswordRequirements
+                <StateWindows 
+                    showConfirm={state.showConfirm}
+                    showError={state.showError}
+                    message={state.message}
+                    container={container}
                 />
-
                 
+                <form onSubmit={handleChangePassword}>
+                    <PasswordInput 
+                        placeholder="Contraseña nueva"
+                        name="user_password"
+                        value={state.form.user_password}
+                        onChange={handleChange}
+                        error={getFieldsError('user_password')}
+                        setError={() => clearFieldsError('user_password', '')}
+                        showPasswordRequirements
+                    />
 
-                <button 
-                    className="cursor-pointer bg-black text-white mt-8 font-bold
-                    w-full py-3 text-sm rounded-md hover:bg-black/80" 
-                    type="submit"
-                >
-                    Cambiar contraseña
-                </button>
-            </form>
+                    <button 
+                        className="cursor-pointer bg-black text-white mt-8 font-bold
+                        w-full py-3 text-sm rounded-md hover:bg-black/80" 
+                        type="submit"
+                    >
+                        Cambiar contraseña
+                    </button>
+                </form>
+            </div>
+
         </div>
     )
 }
