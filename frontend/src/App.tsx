aeeae5dfae3505d70d91c3c0a6c1ec85fab0502e
esotooo <edwinsotos2003@gmail.com>
2025-10-07 12:39:51 -0600

    feat: agregar más rutas para diferentes paginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,13 @@ import {Routes, Route, Navigate} from 'react-router-dom'
 import LoginPage from './Pages/Auth/LoginPage'
 import MainPage from './Pages/Main/MainPage'
 import RegisterUser from './Pages/Auth/RegisterUser'
-import { ProtectedMain } from './Components/Private/ProtectedMain'
+import { ProtectedRoutes } from './Components/Private/ProtectedRoutes'
 import ChangePasswordPages from './features/ChangePasswordPages'
 import VerifyResult from './Pages/Auth/VerifyResult'
+import Profile from './Pages/Main/User/Profile'
+import TasksPage from './Pages/Main/Options/TasksPage'
+import GraphsPage from './Pages/Main/Options/GraphsPage'
+import CalendarPage from './Pages/Main/Options/CalendarPage'
 
 function App() {
 
@@ -15,11 +19,36 @@ function App() {
       <Route path='/change-password' element={<ChangePasswordPages />}/>
       <Route path='/register' element={<RegisterUser />}/>
       <Route path='/verify-result' element={<VerifyResult />}/>
+
       <Route path='/main' element={
-        <ProtectedMain>
+        <ProtectedRoutes>
           <MainPage />
-        </ProtectedMain>
-        } />
+        </ProtectedRoutes>
+      } />
+
+      <Route path='/profile' element={ 
+        <ProtectedRoutes>
+          <Profile />
+        </ProtectedRoutes>
+      }/>
+
+      <Route path='/tasks' element={
+        <ProtectedRoutes>
+          <TasksPage />
+        </ProtectedRoutes>
+      } />
+
+      <Route path='/graphs' element={
+        <ProtectedRoutes>
+          <GraphsPage />
+        </ProtectedRoutes>
+      }/>
+
+      <Route path='/calendar' element={
+        <ProtectedRoutes>
+          <CalendarPage />
+        </ProtectedRoutes>
+      }/>
     </Routes>
   )
 }
