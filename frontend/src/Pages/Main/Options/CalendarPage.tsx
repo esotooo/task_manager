67566bef71a8c26343563f1e5563760cb4fda19f
esotooo <edwinsotos2003@gmail.com>
2025-10-07 12:48:27 -0600

    feat: crear paginas para diferentes opciones disponibles dentro de la aplicación

---
+++ frontend/src/Pages/Main/Options/CalendarPage.tsx
@@ -0,0 +1,8 @@
+
+export default function CalendarPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ frontend/src/Pages/Main/Options/GraphsPage.tsx
@@ -0,0 +1,7 @@
+export default function GraphsPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
