65ee5a380880c2f1f0239f5ae8b1cb52cc9574f5
esotooo <edwinsotos2003@gmail.com>
2025-10-07 12:51:42 -0600

    style: agregar estilos a pagina principal

--- frontend/src/Pages/Main/MainPage.tsx
+++ frontend/src/Pages/Main/MainPage.tsx
@@ -1,11 +1,26 @@
-import { useAuth } from "../../Hooks/Auth/useAuth"
+import DesktopSidebar from "../../Components/Main/DesktopSidebar";
+import MobileOptionsBar from "../../Components/Main/MobileOptionsBar";
+
+
 
 export default function MainPage() {
-    const {logout} = useAuth()
+
 
     return (
-        <div>
-            <button onClick={logout}>Cerrar sesión</button>
+        <div className="block w-screen h-screen md:flex">
+
+            <DesktopSidebar />
+            <MobileOptionsBar />
+
+            {/* Contenido principal */}
+            <div className="flex-1 md:px-12 md:py-8 px-5 py-5 mt-25 md:mt-0">
+                <section className="mb-6">
+                    <h1 className="text-2xl font-bold">Gráficas</h1>
+                </section>
+                <section>
+                    <h1 className="text-2xl font-bold">Tareas</h1>
+                </section>
+            </div>
         </div>
-    )
+    );
 }
