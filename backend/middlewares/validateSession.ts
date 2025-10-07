3d4ad3e0608bba0ea94075b0444dee27da60d506
esotooo <edwinsotos2003@gmail.com>
2025-10-07 12:35:08 -0600

    fix: corregir datos enviados por medio de las cookies y el token

--- backend/middlewares/validateSession.ts
+++ backend/middlewares/validateSession.ts
@@ -1,25 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 import { sendError } from "../utils/responseHandler";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-interface JwtPayload {
-    id: string;
-    email: string;
-  }
-  
-
-  export const validateSession = (req: Request, res: Response, next: NextFunction) => {
+export const validateSession = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.token;
-    if(!token) return sendError(res, 401, "No autenticado");
+    if (!token) return sendError(res, 401, "No autenticado");
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string, email: string };
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+            id_user: string;
+            email: string;
+            firstname: string;
+            lastname: string;
+            username: string;
+        };
+
         (req as any).user = decoded;
         next();
     } catch {
         return sendError(res, 401, "Token inválido");
     }
-}
+};
