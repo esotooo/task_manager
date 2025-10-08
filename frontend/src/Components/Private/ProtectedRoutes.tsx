29505b554ca813f31b30b0bcb033cc74c5406559
esotooo <edwinsotos2003@gmail.com>
2025-10-08 13:42:15 -0600

    fix: corregir redireccionamiento al recargar la pagina, si el usuario esta loggeado le permita recargar y no redirigirlo a main

--- frontend/src/Components/Private/ProtectedRoutes.tsx
+++ frontend/src/Components/Private/ProtectedRoutes.tsx
@@ -1,16 +1,15 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../../Hooks/Auth/useAuth";
 
-type ProtectedRoutesProps = {
-    children: React.JSX.Element
-}
 
-export const ProtectedRoutes = ({ children }: ProtectedRoutesProps) => {
+
+export const ProtectedRoutes = () => {
     const { user } = useAuth()
+    const location = useLocation()
 
     if (!user) {
-        return <Navigate to="/login" replace />
+        return <Navigate to="/login" state={{from: location}} replace />
     }
 
-    return children;
+    return <Outlet />;
 };
