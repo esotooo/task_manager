8e8c5cbab2193a9e15aa5a6b0619db26a60a7a23
esotooo <edwinsotos2003@gmail.com>
2025-10-09 15:33:20 -0600

    feat: agregar endpoints para CRUD de tareas

--- backend/API/tasks/tasksCRUD.ts
+++ backend/API/tasks/tasksCRUD.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import { Request, Response } from "express";
 import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import pool from '../../database/connection';
-import { createTaskQueries } from "../../Queries/Tasks/createTaskQueries";
+import { CRUDqueries } from "../../Queries/Tasks/CRUDqueries";
 import { sendError, sendSuccess } from "../../utils/responseHandler";
+import { GetIDType, GetTaskType } from "../../types/taskTypes";
 
 const router = Router();
-
-export const taskID = [];
 
 router.post('/create-task', async (req: Request, res: Response) => {
     try{
@@ -19,7 +18,7 @@ router.post('/create-task', async (req: Request, res: Response) => {
             id_user, 
         } = req.body
 
-        const [rows] = await pool.query<RowDataPacket[]>(createTaskQueries.obtainTaskID, [id_user])
+        const [rows] = await pool.query<GetIDType[]>(CRUDqueries.obtainTaskID, [id_user])
 
         const lastID = rows.length > 0 ? rows[0].id_task : 0
         const newId = lastID + 1;
@@ -28,7 +27,7 @@ router.post('/create-task', async (req: Request, res: Response) => {
 
         const create_date = new Date();
 
-        const [create] = await pool.query<ResultSetHeader>(createTaskQueries.createTask, [
+        const [create] = await pool.query<ResultSetHeader>(CRUDqueries.createTask, [
             newId, task_title, task_description, id_priority, initialTaskstate, due_date, id_user, create_date
         ]);
 
@@ -53,5 +52,59 @@ router.post('/create-task', async (req: Request, res: Response) => {
     }
 });
 
+
+router.get('/get-tasks', async (req: Request, res: Response) => {
+    try{
+        const {id_user} = req.params;
+
+        const [data] = await pool.query<GetTaskType[]>(CRUDqueries.getTasks, id_user);
+    
+        if(data.length === 0){
+            return sendError(res, 400, 'Aún no cuentas con tareas creadas.');
+        }
+    
+        return sendSuccess(res, 200, data, 'Tareas obtenidas correctamente.');
+    }catch{
+        return sendError(res, 500, 'Error en el servidor.');
+    }
+});
+
+router.put('/update-task', async(req: Request, res:Response) => {
+    try{
+        const {
+            task_title, 
+            task_description,
+            id_priority, 
+            id_state, 
+            due_date, 
+            end_date, 
+            id_task,
+            id_user
+        } = req.body;
+
+        await pool.query<ResultSetHeader>(CRUDqueries.updateTask, [
+            task_title, task_description, id_priority, id_state, due_date, end_date, id_task, id_user
+        ]);
+
+        return sendSuccess(res, 200, [], 'Tarea actualizada exitosamente.');
+
+    }catch{
+        return sendError(res, 500, 'Error en el servidor.');
+    }
+});
+
+router.delete('/delete-task', async(req: Request, res:Response) => {
+    try{
+        const {id_task, id_user} = req.body;
+        
+        const [task] = await pool.query<ResultSetHeader>(CRUDqueries.deleteTask, [id_task, id_user]);
+
+        if(task.affectedRows === 0 ) return sendError(res, 404, "No se encontro la tarea a eliminar.");
+
+        return sendSuccess(res, 200, [], 'Tarea eliminada exitosamente.');
+    }catch{
+        return sendError(res, 500, 'Error en el servidor.');
+    }
+});
 
 export default router;
