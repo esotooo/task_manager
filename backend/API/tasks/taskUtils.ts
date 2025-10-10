a3d05e57e77d7e852c8a3a5f62b1c5069aea9bd0
esotooo <edwinsotos2003@gmail.com>
2025-10-10 17:07:21 -0600

    feat: se agrego tipado para los datos obtenidos desde la base de datos

---
+++ backend/API/tasks/taskUtils.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { Request, Response } from "express";
+import pool from '../../database/connection';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { sendError, sendSuccess } from "../../utils/responseHandler";
+import { utilsQueries } from "../../Queries/Tasks/utilsQueries";
+
+const router = Router();
+
+router.get('/get-priorities', async (req: Request, res: Response) => {
+    try{
+        const [data] = await pool.query<RowDataPacket[]>(utilsQueries.getPriorities);
+        if(data.length === 0){
+            return sendError(res, 400, "No se encontraron registros.");
+        }
+        return sendSuccess(res, 200, data, "Registros obtenidos exitosamente.");
+    }catch{
+        return sendError(res, 500, "Error en el servidor.");
+    }
+});
+
+router.get('/get-states', async (req: Request, res: Response) => {
+    try{
+        const [data] = await pool.query<RowDataPacket[]>(utilsQueries.getStates);
+        if(data.length === 0){
+            return sendError(res, 400, "No se encontraron registros");
+        }
+        return sendSuccess(res, 200, data, "Registros obtenidos exitosamente.");
+    }catch{
+        return sendError(res, 500, "Error en el servidor.");
+    }
+});
+
+export default router;
