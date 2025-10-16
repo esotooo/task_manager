46b496a54cf52346c485f9c9579155bde21051f7
esotooo <edwinsotos2003@gmail.com>
2025-10-15 19:47:10 -0600

    feat: se agrego componente para mostrar la vista principal de la pagina tareas

---
+++ frontend/src/Components/Main/Tasks/MainView.tsx
@@ -0,0 +1,14 @@
+import SearchBar from "./SearchBar"
+import TasksDesktopView from "./TasksDesktopView"
+
+export default function MainView() {
+
+
+    return (
+    <>
+        <SearchBar />
+        <TasksDesktopView />
+    </>
+
+  )
+}
