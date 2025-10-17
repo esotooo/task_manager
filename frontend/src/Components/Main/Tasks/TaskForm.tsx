29738fdd01f4e5dd37a2c10da87f19b15c3835ba
esotooo <edwinsotos2003@gmail.com>
2025-10-17 16:41:13 -0600

    feat: se agrego visualización de titulo segun opcion elegida por el usuario, tambien se desactivaron los campos cuando el usuario selecciona opcion ver

--- frontend/src/Components/Main/Tasks/TaskForm.tsx
+++ frontend/src/Components/Main/Tasks/TaskForm.tsx
@@ -2,13 +2,14 @@ import { useTask } from "../../../Hooks/Tasks/useTask";
 
 export default function TaskForm() {
 
-  const {form, priorities, handleChange, handleCancel, handleSubmit, isEditing, states} = useTask();
+  const {form, priorities, handleChange, handleCancel, handleSubmit, isEditing, states, isViewing } = useTask();
 
   return (
     <section className="mt-6 mx-auto bg-white p-6 rounded-2xl shadow-xl border border-gray-100">
         <h2 className="text-xl font-semibold text-gray-800 mb-4 flex items-center gap-2">
-            {isEditing ? 'Editar Tarea' : 'Agregar Tarea'}
+            {isViewing ? 'Ver Tarea' : isEditing ? 'Editar Tarea'  : 'Agregar Tarea' }
         </h2>
+
 
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             {/* Título */}
@@ -19,8 +20,10 @@ export default function TaskForm() {
                     type="text"
                     name="task_title"
                     value={form.task_title ?? ''}
+                    disabled={isViewing}
                     placeholder="Ej. Corregir bugs del dashboard"
-                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                    className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500
+                        ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
                 />
             </div>
 
@@ -30,8 +33,10 @@ export default function TaskForm() {
                 <select
                     value={form.id_priority ?? ''}
                     name="id_priority"
-                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                    className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500
+                        ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
                     onChange={handleChange}
+                    disabled={isViewing}
                 >
                     <option value="">Seleccionar prioridad</option>
                     {priorities.map(i => (
@@ -40,14 +45,16 @@ export default function TaskForm() {
                 </select>
             </div>
 
-            {isEditing && (
+            {(isEditing || isViewing) &&(
                 <div className="flex flex-col">
                     <label className="text-sm font-medium text-gray-700 mb-1">Estado</label>
                     <select
                         value={form.id_state ?? ''}
                         name="id_state"
-                        className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                        className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500
+                            ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
                         onChange={handleChange}
+                        disabled={isViewing}
                     >
                         <option value="">Seleccionar Estado</option>
                         {states.map(i => (
@@ -66,7 +73,9 @@ export default function TaskForm() {
                     name="task_description"
                     rows={3}
                     placeholder="Agrega detalles o notas sobre la tarea..."
-                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500 resize-none"
+                    className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500 resize-none
+                        ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
+                    disabled={isViewing}
                 ></textarea>
             </div>
 
@@ -78,11 +87,13 @@ export default function TaskForm() {
                     value={form.due_date ?? ''}
                     name="due_date"
                     type="date"
-                    className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                    className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500
+                        ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
+                    disabled={isViewing}
                 />
             </div>
 
-            {isEditing && (
+            {(isEditing || isViewing) &&(
                 <div className="flex flex-col">
                     <label className="text-sm font-medium text-gray-700 mb-1">Fecha Finalización</label>
                     <input
@@ -90,25 +101,31 @@ export default function TaskForm() {
                         value={form.end_date ?? ''}
                         name="end_date"
                         type="date"
-                        className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                        className={`border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-500
+                            ${isViewing ? 'disabled:bg-gray-200 text-gray-500/70 cursor-not-allowed' : ''}`}
+                        disabled={isViewing}
                     />
                 </div>
             )}
 
             {/* Botones */}
             <div className="flex flex-col md:flex-row gap-3 mt-4">
+                {!isViewing && (
+                    <button
+                        className={`bg-amber-500 hover:bg-amber-500/85 px-4 py-2 cursor-pointer text-sm font-semibold text-white rounded-lg transition-all w-full md:w-auto`}
+                        type="submit"
+                        disabled={isViewing}
+                    >
+                        {isEditing ? 'Editar Tarea' : 'Agregar Tarea'}
+                    </button>
+                )}
+
                 <button
-                    className="bg-amber-500 hover:bg-amber-600 px-4 py-2 cursor-pointer text-sm font-semibold text-white rounded-lg transition-all w-full md:w-auto"
-                    type="submit"
-                >
-                    {isEditing ? 'Editar Tarea' : 'Agregar Tarea'}
-                </button>
-                <button
-                    className="bg-gray-800 hover:bg-gray-900 px-4 py-2 cursor-pointer text-sm font-semibold text-white rounded-lg transition-all w-full md:w-auto"
+                    className="bg-[#1A1A1A] hover:bg-[#1A1A1A]/85 px-4 py-2 cursor-pointer text-sm font-semibold text-white rounded-lg transition-all w-full md:w-auto"
                     onClick={handleCancel}
                     type="button"
                 >
-                    Cancelar
+                    {isViewing ? 'Regresar' : 'Cancelar'}
                 </button>
             </div>
         </form>
