343ed2e261e7638cdd9c514f6f5d446c3e84bfbe
esotooo <edwinsotos2003@gmail.com>
2025-10-17 16:38:03 -0600

    feat: se agrego renderización de la ventana modal para confirmar eliminación de tarea

--- frontend/src/Components/Main/Tasks/MainView.tsx
+++ frontend/src/Components/Main/Tasks/MainView.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import TasksDesktopView from "./TasksDesktopView";
 import TasksMobileView from "./TasksMobileView";
+import { useTask } from "../../../Hooks/Tasks/useTask";
+import ConfirmDeleteWindow from "./ConfirmDeleteWindow";
 
 export default function MainView() {
   const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 768);
@@ -15,10 +17,13 @@ export default function MainView() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const {confirmDelete} = useTask()
+
   return (
     <>
       <SearchBar />
       {isMobile ? <TasksMobileView /> : <TasksDesktopView />}
+      {confirmDelete.open && (<ConfirmDeleteWindow />)}
     </>
   );
 }
