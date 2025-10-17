945423d96d561a860685ddc51b57092fa0551b85
esotooo <edwinsotos2003@gmail.com>
2025-10-16 20:19:55 -0600

    feat: se agrego pestaña emergente al pasar el mouse sobre opcion 'ver mas' la cual muestra la descripcion de la tarea. Tambien se agrego boton 'mas opciones' el cual muestra las opciones de ver, editar y eliminar

--- frontend/src/Components/Main/Tasks/TasksDesktopView.tsx
+++ frontend/src/Components/Main/Tasks/TasksDesktopView.tsx
@@ -1,13 +1,16 @@
 import { useTask } from "../../../Hooks/Tasks/useTask"
+import { FaEye } from "react-icons/fa";
+import { HiDotsVertical } from "react-icons/hi";
+
 
 export default function TasksDesktopView() {
 
-    const {tasks, formatDate} = useTask()
-
+    const {tasks, formatDate, toggleRow, openRowId, optionsList, editTask} = useTask()
+    
   return (
-    <div className="mt-6 hidden md:block ">
+    <section className="max-h-[510px] max-w-auto overflow-x-hidden overflow-y-auto">
         <table className="min-w-full">
-        <thead className="border-b border-b-gray-200 text-[16px]">
+        <thead className="sticky top-0 bg-white border-b border-gray-200 z-10">
             <tr>
                 <th className="px-1 py-2">Creado</th>
                 <th className="px-1 py-2">Título</th>
@@ -19,14 +22,29 @@ export default function TasksDesktopView() {
         </thead>
         <tbody>
             {tasks.map((task) => (
-                <tr key={task.id_task} className='hover:bg-gray-100 text-center text-[13px]'>
+                <tr key={task.id_task} className='hover:bg-gray-100 text-center text-[13px] border-b border-b-gray-100'>
                     <td className="px-1 py-2">{formatDate(task.create_date)}</td>
                     <td className="px-1 py-2">{task.task_title}</td>
-                    <td className="px-1 py-2">...</td>
+                    <td className="relative group flex justify-center items-center">
+                    <button
+                        className="flex items-center gap-1 text-gray-500 hover:text-black 
+                        transition-colors duration-200 py-4 "
+                    >
+                        <FaEye />
+                        <span>Ver más</span>
+                    </button>
+                        <div 
+                            className="absolute -left-14 top-full mt-1 hidden group-hover:block 
+                            bg-white border border-gray-100 rounded-lg p-3 shadow-lg z-10 w-80 text-xs
+                            opacity-0 translate-y-2 transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-y-0"
+                        >
+                            {task.task_description}
+                        </div>
+                    </td>
                     <td className="px-1 py-2">{formatDate(task.due_date)}</td>
                     <td className={`px-1 py-2`}>
                         <p 
-                            className={`mx-6 rounded-lg
+                            className={`mx-6 rounded-full
                             ${task.priority === 'Alta' ? 'bg-rose-200' : ''}
                             ${task.priority === 'Media' ? 'bg-amber-200' : ''}
                             ${task.priority === 'Baja' ? 'bg-lime-200' : ''}`}
@@ -36,7 +54,7 @@ export default function TasksDesktopView() {
                     </td>
                     <td className={`px-1 py-2`}>
                         <p 
-                            className={`rounded-lg
+                            className={`rounded-full
                             ${task.state === 'Cancelada' ? 'bg-rose-200' : ''}
                             ${task.state === 'Pendiente' ? 'bg-amber-200' : ''}
                             ${task.state === 'Completada' ? 'bg-lime-200' : ''}
@@ -45,15 +63,33 @@ export default function TasksDesktopView() {
                             {task.state}
                         </p>
                     </td>                        
-                    <td className="px-1 py-2">
-                        <button className="cursor-pointer">
-                            ...
+                    <td className="relative group flex justify-center items-center px-1 py-2">
+                        <button className="cursor-pointer" onClick={() => toggleRow(task.id_task)} type="button">
+                            <HiDotsVertical />
                         </button>
+                        {openRowId === task.id_task && (
+                            <div className="absolute -left-24 top-full mt-1 
+                                bg-white border border-gray-100 rounded-lg p-3 shadow-lg z-10 w-30 text-xs flex flex-col"
+                                ref={optionsList}
+                            >
+                                <button type="button" className="cursor-pointer hover:bg-gray-100 transition-all ease-in-out duration-300 w-full py-2" >
+                                    Ver
+                                </button>
+                                <button type="button" className="cursor-pointer hover:bg-gray-100 transition-all ease-in-out duration-300 w-full py-2" onClick={() => editTask(task.id_task, task.id_user)} >
+                                    Editar
+                                </button>
+                                <button type="button" className="cursor-pointer hover:bg-gray-100 transition-all ease-in-out duration-300 w-full py-2" >
+                                    Eliminar
+                                </button>
+                            </div>
+                        )}
                     </td>
                 </tr>
             ))}
         </tbody>
+ 
         </table>
-    </div>
+
+    </section>
   )
 }
