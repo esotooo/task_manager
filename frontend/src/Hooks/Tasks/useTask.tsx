448c570736d09051241b410175e611d2c79dd3c7
esotooo <edwinsotos2003@gmail.com>
2025-10-16 20:22:07 -0600

    feat: agregar hook useTask para manejar tareas, incluyendo creación, edición, reseteo y cancelación de formulario, conversión de campos numéricos y fechas vacías a null, fetch de tareas, prioridades y estados desde la API, formateo de fechas para visualización, control de filas abiertas en la tabla y notificaciones de éxito/error con react-hot-toast

--- frontend/src/Hooks/Tasks/useTask.tsx
+++ frontend/src/Hooks/Tasks/useTask.tsx
@@ -1,29 +1,69 @@
 import { useAuth } from "../Auth/useAuth"
 import { useTaskStore } from "../../Store/useTaskStore";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import {toast} from 'react-hot-toast'
 
 export function useTask(){
     const { user } = useAuth()
-    const { form, priorities, tasks, message, fetchPriorities, createTask, closeForm, fetchTasks, updateForm, resetForm} = useTaskStore();
+    const { 
+        form,
+        priorities, 
+        tasks, 
+        message, 
+        openRowId,
+        toggleRow,
+        fetchPriorities, 
+        createTask, 
+        closeForm, 
+        fetchTasks, 
+        updateForm, 
+        resetForm,
+        editTask,
+        isEditing,
+        fetchStates,
+        states,
+        updateTask
+    } = useTaskStore();
+
+    const optionsList = useRef<HTMLDivElement | null>(null)
+
 
     function formatDate(dateStr: string) {
         if (!dateStr) return '';
         const date = new Date(dateStr);
-        return date.toLocaleDateString('es-ES'); // 'DD/MM/YYYY'
+        return date.toLocaleDateString('es-ES'); 
     }
+
+    const handleChange = ( e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+      
+        const numericFields = ["id_state", "id_priority"];
+        const newValue = numericFields.includes(name) && value !== "" ? Number(value) : value;
+
+        updateForm(name as keyof typeof form, newValue);
+    };
     
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement> |  React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>  ) => {
-        const {name, value} = e.target
-        updateForm(name as keyof typeof form, value)
-    }
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if(!user?.data.id_user) return;
-    
         try{
-            const msg = await createTask({...form, id_user: user.data.id_user})
+
+            const payload = {
+                ...form,
+                id_user: user.data.id_user,
+                end_date: form.end_date === "" ? null : form.end_date,
+                due_date: form.due_date === "" ? null : form.due_date
+            }
+
+            let msg = ''
+
+            if(isEditing){
+                msg = await updateTask(payload)
+            }else{
+                msg = await createTask(payload)
+            }
+
             toast.success(msg, { duration: 4000, position: "top-right" })
     
             await fetchTasks(user.data.id_user)
@@ -32,7 +72,7 @@ export function useTask(){
                 closeForm()
             }, 1000)
            
-        } catch(err: any) {
+        } catch(err : any) {
             toast.error(err.message, { duration: 4000, position: "top-right" })
         }
     }
@@ -47,11 +87,25 @@ export function useTask(){
     },[fetchPriorities]) 
 
     useEffect(() => {
+        fetchStates()
+    },[fetchStates]) 
+
+    useEffect(() => {
         if (user?.data.id_user) {
-          fetchTasks(user.data.id_user);
+          fetchTasks(user.data.id_user)
         }
-    }, [user?.data.id_user, fetchTasks]);
+    }, [user?.data.id_user, fetchTasks])
       
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if(optionsList.current && !optionsList.current.contains(e.target as Node)){
+                if(openRowId !== null) toggleRow(openRowId)
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside)
+        return() => document.removeEventListener("mousedown", handleClickOutside) 
+    },[openRowId, toggleRow])
 
     return({
         form,
@@ -61,7 +115,13 @@ export function useTask(){
         priorities,
         tasks,
         message,
-        formatDate
+        formatDate,
+        toggleRow,
+        openRowId,
+        optionsList,
+        editTask,
+        isEditing,
+        states
     })    
 }
 
