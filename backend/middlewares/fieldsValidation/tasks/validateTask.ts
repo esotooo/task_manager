4d87f9c4d2eb9eed6604a8319741eb74c9419cd7
esotooo <edwinsotos2003@gmail.com>
2025-10-20 16:45:19 -0600

    refactor: reorganizar middlewares para validación a carpeta para mejor organizacion

---
+++ backend/middlewares/fieldsValidation/tasks/validateTask.ts
@@ -0,0 +1,42 @@
+import {body, validationResult} from 'express-validator';
+
+export const validateTasks = [
+    body('task_title')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese un título para la tarea.')
+        .bail()
+        .isLength({min: 2, max: 50})
+        .withMessage('El título debe contener entre 2 y 50 caracteres.'),
+
+    body('task_description')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese una breve descripción para la tarea.'),
+
+    body('id_priority')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, seleccione una prioridad.')
+        .bail()
+        .matches(/^[0-3]+$/)
+        .withMessage('La prioridad de una tarea es de 0 a 3'),
+
+    body('id_state')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, seleccione un estado.')
+        .bail()
+        .matches(/^[0-4]+$/)
+        .withMessage('El estado de una tarea es de 0 a 4'),
+    
+    body('due_date')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese una fecha de vencimiento.'),
+
+    body('end_date')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese una fecha de completado.')
+]
---
+++ backend/middlewares/fieldsValidation/users/validateChangePassword.ts
@@ -0,0 +1,85 @@
+import {body, validationResult} from 'express-validator';
+import { NextFunction, Request, Response } from 'express';
+import { otpStore } from '../../../API/users/changePassword';
+
+export const validateEmail = [
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Por favor, ingrese su correo electrónico.')
+        .bail()
+        .isEmail()
+        .withMessage('Por favor, ingrese un correo electrónico válido.')
+        .bail(),
+];
+
+export const validateOTP = [
+  body("otp")
+    .trim()
+    .notEmpty()
+    .withMessage("Por favor, ingrese el código de verificación enviado a su correo.")
+    .bail()
+    .isLength({ min: 6, max: 6 })
+    .withMessage("El código debe tener exactamente 6 dígitos.")
+    .bail()
+    .matches(/^[0-9]+$/)
+    .withMessage("El código solo puede contener números.")
+    .custom((value, { req }) => {
+      const emailNormalized = req.body.email.trim().toLowerCase();
+      const storedOTP = otpStore[emailNormalized];
+
+      if (!storedOTP) {
+        throw new Error("No se encontró un código para este correo.");
+      }
+
+      if (Date.now() > storedOTP.expiresAt) {
+        delete otpStore[emailNormalized];
+        throw new Error("El código ha expirado.");
+      }
+
+      if (storedOTP.code !== value) {
+        throw new Error("Código incorrecto.");
+      }
+
+      return true;
+    }),
+];
+
+export const validateNewPassword = [
+    body('user_password')
+        .notEmpty()
+        .withMessage("Por favor, ingrese su contraseña nueva.")
+        .bail()
+        .isLength({min: 8, max: 128})
+        .withMessage('La contraseña debe tener entre 8 y 128 caracteres.')
+        .bail()
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])/)
+        .withMessage('La contraseña ingresada no es válida.'),
+];
+
+
+export const handleValidationErrorsByField = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const errorsByField: Record<string, string> = {};
+        
+        errors.array().forEach((error) => {
+        const field = (error as any).param || (error as any).path || "general";
+        errorsByField[field] = error.msg;
+        });
+
+        const otpExpired = Object.values(errorsByField).some(msg => 
+          msg.includes("El código ha expirado.")
+        )
+
+        const statusCode = otpExpired ? 401 : 400;
+
+        return(res.status(statusCode).json({
+            success: false,
+            fields: errorsByField
+        }));
+    }
+
+    next();
+};
