4d58d3624c74448c6afc9fbd349c0a7496dbff09
esotooo <edwinsotos2003@gmail.com>
2025-11-06 17:22:08 -0600

    feat: se agregaron estilos a la vista de la pagina principal del calendario y se implemento el componente reutilizable Calendar

--- frontend/src/Pages/Main/Options/CalendarPage.tsx
+++ frontend/src/Pages/Main/Options/CalendarPage.tsx
@@ -1,15 +1,19 @@
+import Calendar from "../../../Components/Main/Calendar/Calendar";
 import DesktopSidebar from "../../../Components/Main/DesktopSidebar";
 import MobileOptionsBar from "../../../Components/Main/MobileOptionsBar";
 
 export default function CalendarPage() {
   return (
-    <div className="block w-screen h-screen md:flex">
+    <main className="block w-screen h-screen md:flex">
         <DesktopSidebar />
         <MobileOptionsBar />
 
-        <div className="flex-1 md:px-12 md:py-8 px-5 py-5 mt-25 md:mt-0">
-            <h1 className="text-3xl font-bold">Calendario</h1>
-        </div>
-    </div>
+        <section className="flex-1 md:px-7 md:py-8 px-5 py-5 mt-25 md:mt-0 ">
+            <h1 className="text-3xl font-bold pb-5">Calendario</h1>
+            <div className="flex-1 overflow-auto shadow-lg p-5 border border-gray-100 rounded-lg">
+              <Calendar />
+            </div>
+        </section>
+    </main>
   )
 }
