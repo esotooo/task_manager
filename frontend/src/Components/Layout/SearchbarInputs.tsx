24f10d701c17995d793096ffdd8f66ade0b047b4
esotooo <edwinsotos2003@gmail.com>
2025-11-06 17:16:52 -0600

    feat: se creo el componente reutilizable para la busqueda por medio de fechas

--- frontend/src/Components/Layout/SearchbarInputs.tsx
+++ frontend/src/Components/Layout/SearchbarInputs.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import type { FilterDropdownProps, InputProps } from "../../Types/Layout/SearchbarTypes";
+import type { DateFilterProps, FilterDropdownProps, InputProps } from "../../Types/Layout/SearchbarTypes";
 import { MdOutlineCancel } from "react-icons/md";
 import { IoMdArrowDropdown } from "react-icons/io";
-
+import Calendar from "react-calendar";
 
 export const SearchbarInput: React.FC<InputProps> = ({
     type = 'text',
@@ -27,7 +27,7 @@ export const SearchbarInput: React.FC<InputProps> = ({
             />
             {hasValue && 
                 <MdOutlineCancel 
-                    className="text-gray-400 mr-0 text-lg md:text-sm md:mr-2 cursor-pointer"
+                    className="text-gray-400 mr-0 text-lg md:text-sm md:mr-2 cursor-pointer hover:text-red-500 transition-colors"
                     onClick={cancel}
                 />
             }
@@ -46,43 +46,68 @@ export const FilterDropdown: React.FC<FilterDropdownProps> = ({
     onSelect,
     onClear,
     field,
-    ref
+    ref,
+    isFilterMode = true,
+    disabled = false,
+    className = ''
 }) => {
 
-    const isOpen = optionId === id && !selectedValue;
+    const isOpen = isFilterMode && optionId === id && !selectedValue;
+
+    const handleToggle = () => {
+      if (disabled) return
+      if (isFilterMode && setOptionId) {
+        setOptionId(isOpen ? null : id || null)
+      }
+    }
 
     return(
-        <div className="relative flex-[0.6] md:border-r md:border-gray-100 md:px-2 md:py-2">
+        <div
+            className={`relative w-full md:flex-[0.6] ${
+            isFilterMode ? "md:border-r md:border-gray-100 md:px-2 md:py-2" : ""
+            } ${className}`}
+        >
             <button
-                className="flex items-center justify-between w-full cursor-pointer text-sm font-medium text-gray-700 hover:text-amber-500 transition-colors
-                md:border-none border px-3 py-2 rounded-lg border-gray-200 md:px-0 md:py-0 md:rounded-none"
-                onClick={() => setOptionId(isOpen ? null : id)}
+                className={`flex items-center justify-between w-full text-sm font-medium text-gray-700 hover:text-amber-500 transition-colors
+                    border md:border-none px-3 py-2 md:px-0 md:py-0 rounded-lg border-gray-200 bg-white ${
+                    disabled ? "opacity-60 cursor-not-allowed" : "cursor-pointer"
+                    }`}
+                onClick={handleToggle}
                 id={id}
+                type="button"
+                disabled={disabled}
             >
                 <span>
                     {selectedValue || label}
                 </span>
-                {!selectedValue ? (
-                    <IoMdArrowDropdown
-                        className={`transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
-                    />
-                    ) : (
-                    <MdOutlineCancel
+                {selectedValue ? (
+                    onClear && (
+                        <MdOutlineCancel
                         onClick={(e) => {
-                        e.stopPropagation();
-                        onClear(field);
+                            e.stopPropagation();
+                            onClear(field);
                         }}
-                        className="text-gray-400 hover:text-gray-600"
+                        className="text-gray-400 hover:text-red-500 transition-colors"
+                        />
+                    )
+                    ) : (
+                    <IoMdArrowDropdown
+                        className={`transition-transform duration-200 ${
+                        isOpen ? "rotate-180" : ""
+                        }`}
                     />
-                )}
-            </button>
+                    )}
+                </button>
 
-            {isOpen && !selectedValue &&(
-                <div 
-                    className="absolute md:top-full bottom-full md:bottom-none left-0 w-full mt-1 bg-white border border-gray-200 rounded-lg shadow-lg z-20 md:w-auto min-w-full" 
+                {/* Menú desplegable */}
+                {((isFilterMode && isOpen && !selectedValue) || (!isFilterMode && !disabled)) && (
+                    <div
                     ref={ref}
-                >
-                <ul className="flex flex-col text-sm text-gray-700">
+                    className={`absolute bottom-full md:top-full md:bottom-auto left-0 w-full mt-1 bg-white border border-gray-200 rounded-lg shadow-lg z-20 min-w-full ${
+                        isFilterMode ? "md:top-full md:w-auto" : "top-full"
+                    }`}
+                    >
+                    <ul className="flex flex-col text-sm text-gray-700">
                         {options.map((option) => (
                         <li
                             key={option.id}
@@ -94,8 +119,70 @@ export const FilterDropdown: React.FC<FilterDropdownProps> = ({
                         </li>
                         ))}
                     </ul>
-                </div>
+                    </div>
             )}
         </div>
+    )
+}
+
+export const DateFilter : React.FC<DateFilterProps> = ({
+    label,
+    range,
+    isOpen,
+    calendarRef,
+    onToggle,
+    onChange,
+    onNoLimit,
+    onClear,
+    allowRange = true,
+    allowNoLimit = true,
+}) => {
+    return(
+       <div className="relative md:flex-[1.6]">
+            <button 
+                className="flex items-center justify-between w-full cursor-pointer text-sm font-medium text-gray-700 hover:text-amber-500 transition-colors border md:border-none px-3 py-2 rounded-lg border-gray-200"
+                onClick={onToggle}
+            >
+                <p>
+                    {range?.start_date && range?.end_date
+                        ? `${new Date(range?.start_date).toLocaleDateString("es-ES")} - ${new Date(range?.end_date).toLocaleDateString("es-ES")}`
+                        : range?.start_date
+                        ? new Date(range?.start_date).toLocaleDateString("es-ES")
+                        : label}
+                </p>
+                {range?.start_date || range?.end_date ? (
+                    <MdOutlineCancel
+                        className="text-gray-400 hover:text-red-500 transition-colors"
+                        onClick={onClear}
+                    />
+                    ) : (
+                    <IoMdArrowDropdown
+                        className={`transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
+                    />
+                )}
+            </button>
+            {isOpen && (
+                <div
+                    ref={calendarRef}
+                    className="absolute md:top-full bottom-full left-auto md:left-none md:right-0 md:w-[350px] w-auto mb-2 bg-white border border-gray-200 rounded-lg shadow-lg z-50 p-2 h-fit "
+                >
+                    <Calendar
+                        selectRange={allowRange}
+                        className="w-full rounded-lg border-none shadow-sm hover:shadow-md transition-shadow duration-200"
+                        onChange={onChange}
+                    />
+                    {allowNoLimit && onNoLimit && (
+                        <div className="flex justify-end mt-2 gap-2">
+                            <button
+                                onClick={onNoLimit}
+                                className="text-xs text-gray-600 hover:text-amber-500"
+                            >
+                                Sin límite
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )}
+       </div> 
     )
 }
